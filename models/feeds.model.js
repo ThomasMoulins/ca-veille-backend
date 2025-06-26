07175105f0198cfa78b952e68f73accd21f19d70
ThomasMoulins <13thomasmoulins@gmail.com>
2025-06-26 02:25:20 +0200

    synthax error fix with required

--- models/feeds.model.js
+++ models/feeds.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const feedsSchema = mongoose.Schema({
-    url: { type: String, require: true },
-    name: { type: String, require: true },
+    url: { type: String, required: true },
+    name: { type: String, required: true },
     articles: [{ type: mongoose.Types.ObjectId, ref: "articles" }],
     defaultMedia: { type: String, default: null },
 });
